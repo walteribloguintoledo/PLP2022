ad0a2c110f947921b2973c81f0c83e238c09c4ff
jadesoriano <jadesiguasoriano@gmail.com>
2022-03-18 17:48:54 +0800

    Day5: final

---
+++ Soriano/final/api/functions/functions.php
@@ -0,0 +1,77 @@
+<?php
+
+function find($key, $value) 
+{
+    return ORM::for_table('users')->where($key, $value)->find_one();
+}
+
+function checkUsernameOrEmailDuplicate($username, $email)
+{
+    $error = 0;
+    $message = '';
+
+    if (find('username', $username)) {
+        $error = 1;
+        $message = 'Username is already taken!';
+    } else if (find('email', $email)) {
+        $error = 1;
+        $message = 'Email is already taken!';
+    }
+
+    return array(
+        'error' => $error,
+        'message' => $message
+    );
+}
+
+function update($id, $name, $address, $birthDate, $contactNumber)
+{
+    $person = find('id', $id);
+
+    if (! $person) return false;
+
+    $person->set('name', $name);
+    $person->set('address', $address);
+    $person->set('birth_date', $birthDate);
+    $person->set('contact_number', $contactNumber);
+
+    $person->save();
+}
+
+function retrieve($username)
+{
+    $person = find('username', $username);
+
+    return $person;
+}
+
+function create($name, $username, $email, $address, $birthDate, $contactNumber, $password)
+{
+    $person = ORM::for_table('users')->create();
+    $person->set('name', $name);
+    $person->set('username', $username);
+    $person->set('email', $email);
+    $person->set('address', $address);
+    $person->set('birth_date', $birthDate);
+    $person->set('contact_number', $contactNumber);
+    $person->set('password', encrpyt($password));
+    $person->save();
+}
+
+function encrpytionIV() { return '1234567891011121'; }
+
+function encrpytionKey() { return hex2bin('000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f'); }
+
+function options() { return 0; }
+
+function cipherMethod() { return "AES-128-CTR"; }
+
+function encrpyt($value)
+{
+    return openssl_encrypt($value, cipherMethod(), encrpytionKey(), options(), encrpytionIV());
+}
+
+function decrypt($value)
+{
+    return openssl_decrypt($value, cipherMethod(), encrpytionKey(), options(), encrpytionIV());
+}
---
+++ Soriano/final/api/index.php
@@ -0,0 +1,88 @@
+<?php
+
+include 'functions.loader.php';
+
+$app = new Slim();
+
+$app->post('/update/:id', function ($id) {
+    $appVariables = new AppVariables();
+
+    $result = update($id, $_POST['name'], $_POST['address'], $_POST['birth_date'], $_POST['contact_number']);
+
+    if (! $result) {
+        $appVariables->verified = 1;
+        $appVariables->error = 0;
+        $appVariables->message = "Updated Successfuly";
+    } else {
+        $appVariables->message = "Error";
+    }
+
+    $response = array(
+        "verified" => $appVariables->verified,
+        "error" => $appVariables->error, 
+        "message" => $appVariables->message,
+    );
+
+    echo json_encode($response);
+});
+
+$app->post('/login', function () {
+    $appVariables = new AppVariables();
+
+    $result = retrieve($_POST['username'], $_POST['password']);
+
+    if ($result && decrypt($result->password) === $_POST['password']) {
+        $appVariables->verified = 1;
+        $appVariables->error = 0;
+        $appVariables->message = "Successful";
+        $result = array(
+            "id" => $result->id,
+            "name" => $result->name,
+            "username" => $result->username,
+            "email" => $result->email,
+            "address" => $result->address,
+            "birth_date" => $result->birth_date,
+            "contact_number" => $result->contact_number,
+            // "password" => decrypt($result->password),
+        );
+    } else {
+        $appVariables->message = "Wrong Credentials!";
+    }
+
+    $response = array(
+        "verified" => $appVariables->verified,
+        "error" => $appVariables->error, 
+        "message" => $appVariables->message,
+        "result" => $result,
+    );
+
+    echo json_encode($response);
+});
+
+$app->post('/signup', function () {
+    $appVariables = new AppVariables();
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+
+    $result = checkUsernameOrEmailDuplicate($username, $email);
+
+    if (array_key_exists('error', $result) && $result["error"]) {
+        $appVariables->message = $result["message"];
+    } else {
+        create($_POST['name'], $username, $email, $_POST['address'], $_POST['birth_date'], $_POST['contact_number'], $_POST['password']);
+
+        $appVariables->verified = 1;
+        $appVariables->error = 0;
+        $appVariables->message = "Successful";
+    }
+
+    $response = array(
+        "verified" => $appVariables->verified,
+        "error" => $appVariables->error, 
+        "message" => $appVariables->message
+    );
+
+    echo json_encode($response);
+});
+
+$app->run();
