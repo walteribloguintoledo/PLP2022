6b4e1208ba8872e929cb77c92ad46964e486092c
ricosenpai <ricojaylumiguid0@gmail.com>
2022-03-23 17:40:47 +0800

    Day5 fix

--- Lumiguid/Day5TRy/api/functions/db.php
+++
@@ -1,4 +0,0 @@
-<?php
-ORM::configure('mysql:host=localhost;dbname=blog_barrogo');
-ORM::configure('username', 'root');
-ORM::configure('password', '');
--- Lumiguid/Day5TRy/api/functions/login.php
+++
@@ -1,8 +0,0 @@
-<?php
-  function login($uname, $pass){
-    $query = ORM::for_table('users')->where('username', $uname)->where('password', $pass)->find_many();
-
-    foreach($query as $q){
-      return $user_data = array($q->fullname, $q->username, $q->email, $q->address, $q->birthday, $q->contact);
-    }
-  }
